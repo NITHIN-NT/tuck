243b109441b3156b2df9ee7cb623b09935c208e7
Nithin <nithinraj07sachu@gmail.com>
2025-10-29 23:34:28 +0530

    Product Page Search and Filter and Search + filter is working

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import AdminProductsView,AdminCategoryView
 urlpatterns = [
     path('',views.admin_home,name='admin_home'),
     
@@ -7,11 +8,11 @@ urlpatterns = [
     path('users/edit',views.admin_user_edit,name='admin_user_edit'),
     path('users/add',views.admin_user_add,name='admin_user_add'),
 
-    path('products/',views.admin_products_view,name='admin_products'),
+    path('products/',AdminProductsView.as_view(),name='admin_products'),
     # path('products/edit',views.admin_products_edit,name='admin_products_edit'),
     # path('products/add',views.admin_products_add,name='admin_products_add'),
 
-    path('category/',views.admin_category_view,name='admin_category'),
+    path('category/',AdminCategoryView.as_view(),name='admin_category'),
     # path('category/edit',views.admin_category_edit,name='admin_category_edit'),
     # path('category/add',views.admin_category_add,name='admin_category_add'),
 
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render
+from django.views.generic import TemplateView,DetailView,ListView
+from userFolder.products.models import Product,Category
 # Create your views here.
 def admin_home(request):
     return render(request,'dashboard/dashboard.html')
 
+'''User View Start Here Add Edit View '''
 def admin_user_view(request):
     return render(request,'users/home_user.html')
 
@@ -13,17 +15,49 @@ def admin_user_edit(request):
 def admin_user_add(request):
     return render(request,'users/add_user.html')
 
-def admin_products_view(request):
-    return render(request,'products/products_admin.html')
+'''User View End Here'''
+
+'''Product View Start Here Add Edit View'''
+class AdminProductsView(ListView):
+    model = Product
+    template_name = 'products/products_admin.html'
+    context_object_name = 'products'
+    paginate_by = 10 
+
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+
+        search_query = self.request.GET.get('search','')
+        category_query = self.request.GET.get('category','')
+        status_query = self.request.GET.get('status','')
+
+        if search_query:
+            queryset = queryset.filter(name__icontains=search_query)
+
+        if category_query:
+            queryset = queryset.filter(category__name__icontains=category_query)
+
+        return queryset
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categorys"] = Category.objects.all()
+        context['search_query'] = self.request.GET.get('search', '')
+        context['category_filter'] = self.request.GET.get('category', '')
+        return context
+    
 
 # def admin_user_edit(request):
 #     return render(request,'users/edit_user.html')
 
 # def admin_user_add(request):
 #     return render(request,'users/add_user.html')
-
-def admin_category_view(request):
-    return render(request,'categorys/category.html')
+'''Product View End Here'''
+class AdminCategoryView(ListView):
+    model = Category
+    template_name = 'categorys/category.html'
+    context_object_name = 'categorys'
 
 # def admin_user_edit(request):
 #     return render(request,'users/edit_user.html')
