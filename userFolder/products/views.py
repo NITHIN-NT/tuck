8148c778198942951639a5ff5703991de7dd17c2
Nithin <nithinraj07sachu@gmail.com>
2025-11-05 15:16:44 +0530

    Home Page is now completely Responsive

--- userFolder/products/views.py
+++ userFolder/products/views.py
@@ -34,12 +34,14 @@ class HomePageView(TemplateView):
         most_demanded = Product.objects.filter(is_most_demanded=True).order_by('?')[:4]
         today = timezone.now().date()
         new_arrivals = Product.objects.filter(created_at__date=today).order_by('?')[:4]
-
+        categories = Product.objects.all().distinct()[:4]
 
         context["products"] = products
         context["featured_products"] = featured_products
         context["most_demanded"] = most_demanded
         context["new_arrivals"] = new_arrivals
+        context['categories'] = categories
+
         return context
     
 class AboutView(TemplateView):
